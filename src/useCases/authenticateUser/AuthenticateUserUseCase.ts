0089ac272f7833237b706ed14865037991d0e64d
lucas paiva <lcspaiva87@gmail.com>
2023-12-03 18:23:45 -0300

    Update dependencies and configuration files

--- src/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,4 +1,4 @@
-import { compare } from 'bcrypt'
+import { compare } from 'bcryptjs'
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma'
 import { GenerateRefreshToken } from '../../provider/GenerateRefreshToken'
